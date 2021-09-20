8d8097f502fbcba75ca41fdabaa76cd92e38ede6
Oliver Hader <oliver@typo3.org>
2021-09-20 23:18:03 +0200

    [TASK] Add immutable behavior to Behavior\Attr
    * `Behavior\Attr::addValues()` directly modified instance (existed)
    * `Behavior\Attr::withValues()` returns new instance (new behavior)

--- tests/Behavior/AttrTest.php
+++ tests/Behavior/AttrTest.php
@@ -21,6 +21,51 @@ use TYPO3\HtmlSanitizer\Behavior\DatasetAttrValue;
 
 class AttrTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function addValuesKeepsInstance(): void
+    {
+        $valueA = new DatasetAttrValue('a1', 'a2');
+        $valueB = new DatasetAttrValue('b1', 'b2');
+        $valueC = new DatasetAttrValue('c1', 'c2');
+        $attr = new Attr('test');
+        self::assertSame($attr, $attr->addValues($valueA, $valueB, $valueC));
+    }
+
+    /**
+     * @test
+     */
+    public function withValuesKeepsInstance(): void
+    {
+        $valueA = new DatasetAttrValue('a1', 'a2');
+        $valueB = new DatasetAttrValue('b1', 'b2');
+        $valueC = new DatasetAttrValue('c1', 'c2');
+        $attr = new Attr('test');
+        $attr = $attr->addValues($valueA, $valueB, $valueC);
+        self::assertSame($attr, $attr->withValues($valueA));
+        self::assertSame($attr, $attr->withValues($valueA, $valueB));
+        self::assertSame($attr, $attr->withValues($valueA, $valueB, $valueC));
+    }
+
+    /**
+     * @test
+     */
+    public function withValuesClonesInstance(): void
+    {
+        $valueA = new DatasetAttrValue('a1', 'a2');
+        $valueB = new DatasetAttrValue('b1', 'b2');
+        $valueC = new DatasetAttrValue('c1', 'c2');
+        $attr = new Attr('test');
+        $attr = $attr->addValues($valueA, $valueB, $valueC);
+
+        $valueD = new DatasetAttrValue('d1', 'd2');
+        $valueE = new DatasetAttrValue('e1', 'e2');
+        self::assertNotSame($attr, $attr->withValues($valueD));
+        self::assertNotSame($attr, $attr->withValues($valueD, $valueE));
+        self::assertNotSame($attr, $attr->withValues($valueA, $valueD));
+    }
+
     public static function matchesNameDataProvider(): array
     {
         return [
