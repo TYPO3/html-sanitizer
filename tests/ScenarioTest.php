2af647676e5e34c21e6e1adfb9257acc5ba5aa52
Oliver Hader <oliver.hader@typo3.org>
2026-04-29 11:15:23 +0200

    [!!!][TASK] Require Behavior and Visitors in Sanitizer constructor
    This requirement has been implemented in TYPO3 v10 already in 2022,
    but was forgotten to be enforced in the typo3/html-sanitizer library.
    see https://review.typo3.org/c/Packages/TYPO3.CMS/+/76848
    
    Fixes: #101
    Related: #100

--- tests/ScenarioTest.php
+++ tests/ScenarioTest.php
@@ -26,22 +26,6 @@ use TYPO3\HtmlSanitizer\Visitor\CommonVisitor;
 
 class ScenarioTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function missingBehaviorTriggersDeprecationError(): void
-    {
-        $this->markTestSkipped('see https://github.com/TYPO3/html-sanitizer/issues/99');
-
-        $this->expectDeprecation();
-        $this->expectDeprecationMessage(
-            'Add `Behavior` when creating new `Sanitizer` instances, e.g. `new Sanitizer($behavior, $visitor)`'
-        );
-        $behavior = new Behavior();
-        $visitor = new CommonVisitor($behavior);
-        new Sanitizer($visitor);
-    }
-
     public static function allTagsAreRemovedOnMissingDeclarationDataProvider(): array
     {
         return [
